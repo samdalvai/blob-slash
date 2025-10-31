2531e9a61a9df3b75332aa706b19c7f92535f8e6
samdalvai <samuel.dalvai@gmail.com>
2025-10-31 16:49:46 +0100

    feat: add logic for dropping health globes on death

--- src/game/systems/DropItemSystem.ts
+++ src/game/systems/DropItemSystem.ts
@@ -1,6 +1,15 @@
 import System from '../../engine/ecs/System';
 import EventBus from '../../engine/event-bus/EventBus';
-import { DropItemOnDeathComponent } from '../components';
+import {
+    AnimationComponent,
+    BoxColliderComponent,
+    DropItemOnDeathComponent,
+    LifetimeComponent,
+    PickableItemComponent,
+    ShadowComponent,
+    SpriteComponent,
+    TransformComponent,
+} from '../components';
 import EntityKilledEvent from '../events/EntityKilledEvent';
 
 export default class DropItemSystem extends System {
@@ -14,6 +23,44 @@ export default class DropItemSystem extends System {
     }
 
     onEntityDeath = (event: EntityKilledEvent) => {
-        // Do something
+        if (event.entity.hasComponent(DropItemOnDeathComponent)) {
+            const dropItemOnDeath = event.entity.getComponent(DropItemOnDeathComponent);
+
+            if (!dropItemOnDeath) {
+                throw new Error('Could not find DropItemOnDeathComponent of entity with id ' + event.entity.getId());
+            }
+
+            const randomValue = Math.random() * 100;
+
+            if (randomValue < dropItemOnDeath.dropPercentage) {
+                switch (dropItemOnDeath.droppedItem) {
+                    case 'health': {
+                        const transform = event.entity.getComponent(TransformComponent);
+
+                        if (!transform) {
+                            throw new Error(
+                                'Could not find TransformComponent of entity with id ' + event.entity.getId(),
+                            );
+                        }
+
+                        const healthGlobe = event.entity.registry.createEntity();
+                        healthGlobe.addComponent(TransformComponent, {
+                            x: transform.position.x,
+                            y: transform.position.y,
+                        });
+                        healthGlobe.addComponent(SpriteComponent, 'health_globe', 32, 32, 2);
+                        healthGlobe.addComponent(AnimationComponent, 4, 10);
+                        healthGlobe.addComponent(ShadowComponent, 28, 16);
+                        healthGlobe.addComponent(BoxColliderComponent, 32, 32);
+                        healthGlobe.addComponent(PickableItemComponent, 'health', 50);
+                        healthGlobe.addComponent(LifetimeComponent, 60000);
+
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+        }
     };
 }
