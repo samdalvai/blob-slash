5cbd23e4a5b99ea2fa8a8285c7679143b967da90
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-30 22:00:00 +0200

    feat: add empty level with one player

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,4 +1,5 @@
-import { Engine, GameStatus } from '../engine';
+import { Engine, Entity, GameStatus } from '../engine';
+import { AnimationComponent, PlayerControlComponent, RigidBodyComponent, SpriteComponent, TransformComponent } from './components';
 import { gameComponentCatalog } from './components/componentCatalog';
 import * as GameEvents from './events';
 import * as Systems from './systems';
@@ -43,7 +44,7 @@ export default class Game extends Engine {
         this.registry.addSystem(Systems.EntityHighlightSystem);
         this.registry.addSystem(Systems.EntityEffectSystem);
         this.registry.addSystem(Systems.AnimationOnHitSystem);
-        this.registry.addSystem(Systems.GameEndSystem);
+        // this.registry.addSystem(Systems.GameEndSystem);
         this.registry.addSystem(Systems.DropItemSystem);
         this.registry.addSystem(Systems.PickItemSystem);
 
@@ -59,8 +60,20 @@ export default class Game extends Engine {
         // await this.levelManager.addLevelToAssets('snapshot', '/assets/levels/snapshot.json');
         // await this.levelManager.loadLevelFromAssets('snapshot');
 
-        await this.levelManager.addLevelToAssets('grass', '/assets/levels/grass.json');
-        await this.levelManager.loadLevelFromAssets('grass');
+        // await this.levelManager.addLevelToAssets('grass', '/assets/levels/grass.json');
+        // await this.levelManager.loadLevelFromAssets('grass');
+        const defaultLevel = this.levelManager.getDefaultLevel('0');
+        await this.levelManager.loadLevelFromLevelMap(defaultLevel.level);
+
+        this.assetStore.addTexture('slime', './assets/sprites/slime_big_full.png');
+
+        const player = this.registry.createEntity();
+        player.addComponent(TransformComponent, { x: 100, y: 100 }, { x: 1, y: 1 }, 0);
+        player.addComponent(SpriteComponent, 'slime', 32, 32, 3);
+        player.addComponent(RigidBodyComponent);
+        player.addComponent(PlayerControlComponent, 100);
+        player.addComponent(AnimationComponent, 2, 4);
+        player.tag('player');
 
         // await this.levelManager.addLevelToAssets('test', '/assets/levels/test.json');
         // await this.levelManager.loadLevelFromAssets('test');
