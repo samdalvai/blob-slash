e5b78c2b1c0817b5c40926673b310f2743190f22
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-07-20 20:00:00 +0200

    feat: add todos for entities synchronization

--- src/game/systems/PhysicsSystem.ts
+++ src/game/systems/PhysicsSystem.ts
@@ -4,15 +4,24 @@ import { RigidBodyComponent } from '../components';
 
 export default class PhysicsSystem extends System {
     private world: World;
+    private entityIdToPhysicsId: Map<number, number>;
 
     constructor() {
         super();
         super.requireComponent(RigidBodyComponent);
 
         this.world = new World(9.8);
+        this.entityIdToPhysicsId = new Map();
     }
 
     update(deltaTime: number) {
+        // Add entities added in engine but missing from physcis system
+        // ...
+
+
+        // Remove entities present in physics system but removed from engine
+        // ...
+
         // Update physics
         let accumulator = 0;
         this.world.update(deltaTime);
